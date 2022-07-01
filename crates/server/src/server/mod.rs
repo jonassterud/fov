65f7f880ae4074efd40e42a23abced1a34c64529
Jonas Mathisrud Sterud <jonas.sterud@gmail.com>
2022-07-02 00:04:21 +0200

    feat: basic server logic

--- crates/server/src/server/mod.rs
+++ crates/server/src/server/mod.rs
@@ -2,16 +2,37 @@ mod portfolio;
 
 use anyhow::Result;
 use portfolio::Portfolio;
+use warp::Filter;
 
 #[derive(Debug)]
-pub struct Server {}
+pub struct Server {
+    portfolio: Portfolio,
+}
 
 impl Server {
     pub fn new() -> Server {
-        Server {}
+        Server {
+            portfolio: Portfolio::new(),
+        }
     }
 
     pub async fn start(self) -> Result<()> {
-        todo!("Start server");
+        let cbp_assets = warp::path!("cbp" / "assets").map(move || {
+            serde_json::to_string(&self.portfolio.cbp_assets).expect("Failed serializing Asset")
+        });
+
+        let nn_assets = warp::path!("nn" / "assets").map(move || {
+            serde_json::to_string(&self.portfolio.nn_assets).expect("Failed serializing Asset")
+        });
+
+        let sb1_assets = warp::path!("sb1" / "assets").map(move || {
+            serde_json::to_string(&self.portfolio.sb1_assets).expect("Failed serializing Asset")
+        });
+
+        let routes = warp::get().and(cbp_assets.or(nn_assets).or(sb1_assets));
+
+        warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+
+        Ok(())
     }
 }
