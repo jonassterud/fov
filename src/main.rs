e93674b9945192fd9b610713aee8239b4513259b
Jonas Mathisrud Sterud <jonas.sterud@gmail.com>
2022-07-03 00:22:39 -0400

    feat: move server into seperate thread

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,16 @@ use shared::Config;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    // TODO: Move this into a seperate thread
     // Open config and start server
-    let config = Config::from_file("/home/jonassterud/Documents/fov/src/secret.toml")?;
-    Server::new(config).start().await?;
+    let server_handle = tokio::spawn(async {
+        let config = Config::from_file("/home/jonassterud/Documents/fov/src/secret.toml").expect("Failed opening config");
+        Server::new(config).start().await.expect("Server failed");
+    });
 
     // Start web app
     // ...
+
+    server_handle.await?;
 
     Ok(())
 }
