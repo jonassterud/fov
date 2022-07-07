4152db0a4f1392519a180ffded13a5bfbdc9a69d
Jonas Mathisrud Sterud <jonas.sterud@gmail.com>
2022-07-08 00:14:55 +0200

    Pie chart + "totals" row (#7)
    * feat: calculate total value
    
    * feat: pie chart
    
    * feat: skip assets with very low procentage
    
    * feat: improve javascript

--- src/www/script.js
+++ src/www/script.js
@@ -1,38 +1,21 @@
+// Global variables
+var total_value = 0;
+var assets = [];
+
+// Events
 window.onload = () => {
-    load_all_assets();
-}
-
-function load_all_assets() {
-    load_sb1_assets();
-    load_cbp_assets();
-    load_nn_assets();
-    // ...
-}
-
-function load_sb1_assets() {
-    load_assets("sb1/assets").then(data => {
-        add_assets_to_table(data, "SpareBank 1")
+    Promise.all([
+        load_assets("sb1/assets", "SpareBank 1"),
+        load_assets("cbp/assets", "Coinbase Pro"),
+        load_assets("nn/assets", "Nordnet"),
+    ]).then(() => {
+        create_diversification_chart();
     }).catch(error => {
         console.error(error);
     });
 }
 
-function load_cbp_assets() {
-    load_assets("cbp/assets").then(data => {
-        add_assets_to_table(data, "Coinbase Pro")
-    }).catch(error => {
-        console.error(error);
-    });
-}
-
-function load_nn_assets() {
-    load_assets("nn/assets").then(data => {
-        add_assets_to_table(data, "Nordnet")
-    }).catch(error => {
-        console.error(error);
-    });
-}
-
+// Asset functions
 function add_assets_to_table(data, title) {
     let table_body = document.getElementById("asset_table");
     if (table_body === null) {
@@ -45,6 +28,8 @@ function add_assets_to_table(data, title) {
 
     // Add assets
     data.forEach(asset => {
+        assets.push(asset);
+
         // Add name and description together
         let name = asset.name + (asset.description ? ` - ${asset.description}` : "");
         // Only show ticker if currency/ticker is not NOK
@@ -53,7 +38,9 @@ function add_assets_to_table(data, title) {
         let balance = asset.currency === "NOK" ? "" : asset.balance;
         // Calculate value of asset in NOK
         let value = Math.round(asset.value) + " NOK";
+        total_value += asset.value;
 
+        // Add asset into table 
         let table_row = `
         <tr>
             <td headers="name">${name}</td>
@@ -64,15 +51,67 @@ function add_assets_to_table(data, title) {
 
         table_body.innerHTML += table_row;
     });
+
+    // Update total value
+    let total_value_cell = document.getElementById("total_value");
+
+    if (total_value_cell === null) {
+        throw new Error("Failed getting td with id: total_value");
+    }
+
+    total_value_cell.innerHTML = Math.round(total_value) + " NOK";
 }
 
-function load_assets(path) {
+function load_assets(path, title) {
     return fetch(window.location.href + path)
         .then(res => {
             if (!res.ok) {
                 throw new Error(`API request failed with status ${res.status}`);
+            } else {
+                return res.json().then(data => add_assets_to_table(data, title));
             }
+        });
+}
 
-            return res.json();
-        })
+// Diversification functions
+function create_diversification_chart() {
+    let diversification = document.getElementById("diversification");
+    let ul = document.getElementById("diversification_overview");
+    let c = document.getElementById("diversification_chart");
+    let cx = c.getContext("2d");
+
+    if (diversification === null || c === null || ul === null) {
+        return new Error("Failed getting one or more HTML elements");
+    }
+
+    let list_items = "";
+    let prev_angle = 0;
+    assets.forEach(asset => {
+        let name = asset.name;
+        let procentage = asset.value / total_value;
+
+        // Skip assets with a too low procentage
+        if (procentage <= 0.01) return;
+
+        let angle = procentage * Math.PI * 2;
+        let random_color = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+        
+        // Create list item
+        list_items += `<li><span style="color: ${random_color}">▣  </span>${name}</li>`;
+
+        // Draw pie
+        cx.strokeStyle = "white";
+        cx.fillStyle = random_color;
+        cx.beginPath();
+        cx.arc(c.width/2, c.height/2, c.width/2, prev_angle, prev_angle + angle, false);
+        cx.lineTo(c.width/2, c.height/2);
+        cx.closePath();
+        cx.fill();
+        cx.stroke();
+
+        prev_angle += angle;
+    });
+
+    // Add to list
+    ul.innerHTML += list_items;
 }
