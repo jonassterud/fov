6c9d82e4e31bacea6fb62a16602f1d94c92ff30b
Jonas Mathisrud Sterud <jonas.sterud@gmail.com>
2022-07-09 22:35:09 +0000

    fix: use relative paths

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,8 @@ use shared::Config;
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     // Open config and start server
     let server_handle = tokio::spawn(async {
-        let config_path = PathBuf::from("/home/jonassterud/Documents/fov/src/config.toml");
-        let secret_path = PathBuf::from("/home/jonassterud/Documents/fov/src/secret.toml");
+        let config_path = PathBuf::from("./src/config.toml");
+        let secret_path = PathBuf::from("./src/secret.toml");
 
         let password = Password::new().with_prompt("Enter password").interact().unwrap();
         let config = Config::open(&config_path, &secret_path, &password).unwrap();
