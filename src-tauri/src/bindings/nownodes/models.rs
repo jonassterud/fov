8a830f1a31b3eaa5c2bd1db969ed9cf511af88cd
Jonas Mathisrud Sterud <jonas.sterud@gmail.com>
2022-07-27 11:48:09 +0000

    fix(temp): missing fields

--- src-tauri/src/bindings/nownodes/models.rs
+++ src-tauri/src/bindings/nownodes/models.rs
@@ -7,7 +7,7 @@ pub struct BtcUtxo {
     pub vout: u32,
     pub value: String,
     pub confirmations: u32,
-    pub script_pub_key: String,
+    pub script_pub_key: Option<String>, // missing?
 }
 
 #[derive(Deserialize, Clone, Debug)]
@@ -18,6 +18,6 @@ pub struct LtcUtxo {
     pub value: String,
     pub height: u64,
     pub confirmations: u32,
-    pub coinbase: bool,
-    pub script_pub_key: String,
+    pub coinbase: Option<bool>, // missing?
+    pub script_pub_key: Option<String>, // missing?
 }
